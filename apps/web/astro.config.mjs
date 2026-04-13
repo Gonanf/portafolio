90b155f5755fa7c09be9d4381c57a52b2fcb2f61
Chaos <gabysolotorevsky100@gmail.com>
2026-04-13 19:32:37 -0300

    (feature) Updated proyects and deployment

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,13 +1,17 @@
 // @ts-check
-
+import cloudflare from "@astrojs/cloudflare";
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
 import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
+	output: 'server',
   vite: {
     plugins: [tailwindcss()],
   },
   integrations: [react()],
+adapter: cloudflare({
+imageService: { build: 'compile', runtime: 'cloudflare-binding' },
+}),
 })
